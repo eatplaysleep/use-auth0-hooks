1d260b086e07c3888dcc234378b20947fbc427d5
Danny Fuhriman <eatplaysleep@gmail.com>
2020-12-01 15:12:17 -0700

    Update next.config.js

--- examples/nextjs-spa/next.config.js
+++ examples/nextjs-spa/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     AUTH0_DOMAIN: 'identity-dev.mgmresorts.com/oauth2/ausk4d0yoLYEONwH04x6/v1',
     ISSUER: 'identity-dev.mgmresorts.com/oauth2/ausk4d0yoLYEONwH04x6',
     AUTH0_CLIENT_ID: 'mgm_app_web',
-    REDIRECT_URI: 'http://localhost:3000/',
-    POST_LOGOUT_REDIRECT_URI: 'http://localhost:3000/'
+    REDIRECT_URI: 'https://next-js-okta-auth.herokuapp.com',
+    POST_LOGOUT_REDIRECT_URI: 'https://next-js-okta-auth.herokuapp.com'
   }
 }
