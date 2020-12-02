00c0195fd4fc097a7d20dfe9bc9f8d124f914e70
Danny Fuhriman <eatplaysleep@gmail.com>
2020-12-01 21:14:02 -0700

    Update next.config.js

--- examples/nextjs-spa/next.config.js
+++ examples/nextjs-spa/next.config.js
@@ -9,3 +9,19 @@ module.exports = {
     POST_LOGOUT_REDIRECT_URI: 'https://use-auth0-hooks-example.vercel.app/'
   }
 }
+
+module.exports = {
+  async Headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ]
+      }
+    ]
+  }
+}
