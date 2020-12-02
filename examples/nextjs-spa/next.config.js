fcfc7243508321a67d7ffada51bc4cc70609834d
Danny Fuhriman <eatplaysleep@gmail.com>
2020-12-01 20:52:43 -0700

    Update next.config.js

--- examples/nextjs-spa/next.config.js
+++ examples/nextjs-spa/next.config.js
@@ -5,12 +5,7 @@ module.exports = {
     AUTH0_DOMAIN: 'identity-dev.mgmresorts.com/oauth2/ausk4d0yoLYEONwH04x6/v1',
     ISSUER: 'identity-dev.mgmresorts.com/oauth2/ausk4d0yoLYEONwH04x6',
     AUTH0_CLIENT_ID: 'mgm_app_web',
-<<<<<<< Updated upstream
-    REDIRECT_URI: 'https://next-js-okta-auth.herokuapp.com',
-    POST_LOGOUT_REDIRECT_URI: 'https://next-js-okta-auth.herokuapp.com'
-=======
     REDIRECT_URI: 'https://use-auth0-hooks-example.vercel.app/',
     POST_LOGOUT_REDIRECT_URI: 'https://use-auth0-hooks-example.vercel.app/'
->>>>>>> Stashed changes
   }
 }
